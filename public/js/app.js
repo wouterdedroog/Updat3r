d8f901012e4efea5775605420d79719dca36cdcd
MrWouterNL <mail@mrwouter.nl>
2020-09-12 11:17:11 +0200

    Add .idea folder to gitignore

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\updat3r\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\updat3r\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\WouterJdeDroog\Desktop\Updat3r\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\WouterJdeDroog\Desktop\Updat3r\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
