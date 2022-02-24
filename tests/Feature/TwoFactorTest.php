d88618f060462a0be78d899fadab5d8601420bc2
MrWouterNL <wouter@dedroog.com>
2022-02-24 22:21:56 +0100

    Add 2FA login tests

--- tests/Feature/TwoFactorTest.php
+++ tests/Feature/TwoFactorTest.php
@@ -2,6 +2,8 @@
 
 use App\Models\TwoFactorMethod;
 use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Hash;
 use PragmaRX\Google2FAQRCode\Google2FA;
 use function Pest\Faker\faker;
 
@@ -53,3 +55,37 @@ it('is possible to delete two factor methods', function () {
 
     expect($user->fresh()->twoFactorMethods)->toBeEmpty();
 });
+
+it('is required to enter a 2FA when logging in with 2FA enabled', function () {
+    $user = User::factory()->has(TwoFactorMethod::factory())->create([
+        'email' => 'wouter@example.com',
+        'password' => Hash::make('Welkom01!')
+    ]);
+
+    $this->actingAs($user)
+        ->get(route('dashboard'))
+        ->assertViewIs('2fa.enter_otp');
+});
+
+it('is possible to login with a valid 2FA code', function () {
+    $user = User::factory()->has(TwoFactorMethod::factory())->create();
+    $secret = decrypt($user->twoFactorMethods->first()->google2fa_secret);
+    $google2fa = new Google2FA();
+    $otp = $google2fa->getCurrentOtp($secret);
+
+    $this->actingAs($user)
+        ->post(route('2fa.verify_otp'), ['otp' => $otp])
+        ->assertSessionMissing('error')
+        ->assertSessionHasNoErrors()
+        ->assertRedirect(RouteServiceProvider::HOME);
+});
+
+it('isn\'t possible to login with an invalid 2FA code', function () {
+    $user = User::factory()->has(TwoFactorMethod::factory())->create();
+
+    $this->actingAs($user)
+        ->post(route('2fa.verify_otp'), ['otp' => (string) rand(100000, 999999)])
+        ->assertSessionHas('error')
+        ->assertRedirect(RouteServiceProvider::HOME);
+});
+
